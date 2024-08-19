92fab01474841dabb59b17befd3d8d84963770b3
Eetu Rantanen <eetu@eeturantanen.fi>
2024-08-19 23:28:11 +0300

    wip

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -14,15 +14,15 @@
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
-    <body class="font-sans text-gray-900 antialiased">
-        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
+    <body class="font-sans antialiased text-white">
+        <div class="flex flex-col items-center min-h-screen pt-6 sm:justify-center sm:pt-0 bg-base-900">
             <div>
                 <a href="/" wire:navigate>
-                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+                    <x-application-logo class="w-20 h-20 fill-current text-primary" />
                 </a>
             </div>
 
-            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
+            <div class="w-full px-6 py-4 mt-6 overflow-hidden border shadow-md bg-base-700 border-primary/50 sm:max-w-md sm:rounded-lg">
                 {{ $slot }}
             </div>
         </div>
